24b553c49d36dbd92ad1ee5c2287e1911dd5f1df
bingoo <bingoo.huang@gmail.com>
2018-07-11 20:43:08 +0800

    update deploy scripts

--- deploy-test.sh
+++ deploy-test.sh
@@ -3,18 +3,21 @@
 # ./deploy.sh yogaapp@test.ino01
 targetHost=$1
 deployName=go-yoga-proxy
+fast=$2
 
-rm -fr $deployName.linux.bin $deployName.linux.bin.bz2
-env GOOS=linux GOARCH=amd64 go build -o $deployName.linux.bin
-bzip2 $deployName.linux.bin
-rsync -avz --human-readable --progress -e "ssh -p 22" ./$deployName.linux.bin.bz2 $targetHost:./app/$deployName/
+if [ "$fast" == "fast" ]; then
+    echo "jump building in fast mode"
+else
+    echo "rebuilding"
+    env GOOS=linux GOARCH=amd64 go build -o $deployName.linux.bin
+    upx $deployName.linux.bin
+fi
+
+rsync -avz --human-readable --progress -e "ssh -p 22" ./$deployName.linux.bin $targetHost:.
 ssh -tt $targetHost "bash -s" << eeooff
 cd ./app/$deployName/
 ps -ef|grep $deployName|grep -v grep|awk '{print \$2}'|xargs -r kill -9
-rm $deployName.linux.bin
-bzip2 -d $deployName.linux.bin.bz2
-./start-$deployName.linux.sh
+mv -f ~/$deployName.linux.bin .
+nohup ./$deployName.linux.bin -redisAddr=127.0.0.1:8051 > $deployName.out 2>&1 &
 exit
 eeooff
-
-rm -fr $deployName.linux.bin.bz2
